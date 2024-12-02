c85eabd48438318442620578860e61a8d98f1bdf
Joe <josephaddunne@gmail.com>
2024-12-02 12:40:55 +0000

    Added module files and main which calls the LZWdecode function

---
+++ lzw/main/go.mod
@@ -0,0 +1,7 @@
+module main
+
+go 1.23.3
+
+replace lzw/decode => ../decode
+
+require lzw/decode v0.0.0-00010101000000-000000000000
---
+++ lzw/main/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"lzw/decode"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+
+	fileName, err := parseArgs()
+	if err != nil {
+		fmt.Println("Arguments Error:", err)
+		return
+	}
+
+	decodedBytes, err := decode.LZWDecode(fileName)
+	if err != nil {
+		fmt.Println("Error decoding file:", err)
+		return
+	}
+	fmt.Printf("Decoded file: %s\n", fileName)
+
+	err = writeToFile(fileName, decodedBytes)
+	if err != nil {
+		fmt.Println("Failed to write to file:", err)
+		return
+	}
+	fmt.Printf("Decoded file written to: %s\n", fileName)
+
+}
+
+func parseArgs() (string, error) {
+	if len(os.Args) < 2 {
+		return "", errors.New("needs a filename argument")
+	}
+	if len(os.Args) > 2 {
+		return "", errors.New("only one argument required for filename")
+	}
+
+	return os.Args[1], nil
+
+}
+
+func writeToFile(fileName string, decodedBytes []byte) error {
+
+	// get the name, and remove the .z extension
+	base := filepath.Base(fileName)
+	nameWithoutExt := strings.TrimSuffix(base, ".z")
+	decodedFile := "decoded-" + nameWithoutExt
+
+	file, err := os.Create(decodedFile)
+
+	if err != nil {
+		return errors.New("could not make a new file")
+	}
+	defer file.Close()
+
+	_, err = file.Write(decodedBytes)
+	if err != nil {
+		return errors.New("could not write bytes to new file")
+	}
+	return nil
+}
